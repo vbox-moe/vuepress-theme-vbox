8110922ea87adcc21d91eab79d6bd40e7ac5c64f
Il Harper <afanyiyu@hotmail.com>
2020-07-01 19:18:40 +0800

    feat: Add MavonEditor Component

---
+++ components/Editor.vue
@@ -0,0 +1,21 @@
+<template>
+  <mavon-editor />
+</template>
+
+<script>
+import { mavonEditor } from '@theme/components/MavonEditor/src/index.js'
+import '@theme/components/MavonEditor/src/css/markdown.css'
+import '@theme/components/MavonEditor/src/css/mavon-editor.css'
+import '@theme/components/MavonEditor/src/css/md.css'
+import '@theme/components/MavonEditor/src/css/scroll.css'
+
+export default {
+  name: 'Editor',
+  components: {
+    mavonEditor
+  }
+}
+</script>
+
+<style>
+</style>
