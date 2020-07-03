b74d26bca617dc07014a65efbb0cdcfe43ec0069
Il Harper <afanyiyu@hotmail.com>
2020-07-03 10:23:47 +0800

    feat: Layout Update

--- components/Page.vue
+++ components/Page.vue
@@ -2,7 +2,11 @@
   <main class="page">
     <slot name="top" />
 
-    <Content class="theme-default-content" />
+    <div class="theme-default-content">
+      <PageHeader />
+      <Content />
+    </div>
+
     <PageEdit />
 
     <PageNav v-bind="{ sidebarItems }" />
@@ -12,11 +16,12 @@
 </template>
 
 <script>
+import PageHeader from '@theme/components/PageHeader.vue'
 import PageEdit from '@theme/components/PageEdit.vue'
 import PageNav from '@theme/components/PageNav.vue'
 
 export default {
-  components: { PageEdit, PageNav },
+  components: { PageHeader, PageEdit, PageNav },
   props: ['sidebarItems']
 }
 </script>
---
+++ components/PageHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>
+      <a :href="'#' + $page.title" class="header-anchor">#</a>
+      {{ $page.title }}
+    </h1>
+    <p class="header-bar">
+      {{ $page.readingTime.text }}
+      ·
+      {{ $page.readingTime.words }} 字 ·
+      <img
+        :alt="author.name"
+        :key="author.email"
+        v-for="author in $page.authors"
+      />
+      ·
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.header-bar {
+  color: #818a94;
+}
+</style>
