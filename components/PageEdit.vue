10a0b7fbdf49232770077a558a5fbfe7f1d3d18b
Il Harper <afanyiyu@hotmail.com>
2020-07-08 15:00:40 +0800

    feat: PageEdit Update

--- components/PageEdit.vue
+++ components/PageEdit.vue
@@ -37,27 +37,7 @@ export default {
     },
 
     editLink() {
-      const showEditLink = isNil(this.$page.frontmatter.editLink)
-        ? this.$site.themeConfig.editLinks
-        : this.$page.frontmatter.editLink
-
-      const {
-        repo,
-        docsDir = '',
-        docsBranch = 'master',
-        docsRepo = repo
-      } = this.$site.themeConfig
-
-      if (showEditLink && docsRepo && this.$page.relativePath) {
-        return this.createEditLink(
-          repo,
-          docsRepo,
-          docsDir,
-          docsBranch,
-          this.$page.relativePath
-        )
-      }
-      return null
+      return '/editor/?file=/' + this.$page.relativePath
     },
 
     editLinkText() {
@@ -67,47 +47,17 @@ export default {
         `Edit this page`
       )
     }
-  },
-
-  methods: {
-    createEditLink(repo, docsRepo, docsDir, docsBranch, path) {
-      const bitbucket = /bitbucket.org/
-      if (bitbucket.test(repo)) {
-        const base = outboundRE.test(docsRepo) ? docsRepo : repo
-        return (
-          base.replace(endingSlashRE, '') +
-          `/src` +
-          `/${docsBranch}/` +
-          (docsDir ? docsDir.replace(endingSlashRE, '') + '/' : '') +
-          path +
-          `?mode=edit&spa=0&at=${docsBranch}&fileviewer=file-view-default`
-        )
-      }
-
-      const base = outboundRE.test(docsRepo)
-        ? docsRepo
-        : `https://github.com/${docsRepo}`
-      return (
-        base.replace(endingSlashRE, '') +
-        `/edit` +
-        `/${docsBranch}/` +
-        (docsDir ? docsDir.replace(endingSlashRE, '') + '/' : '') +
-        path
-      )
-    }
   }
 }
 </script>
 
 <style lang="stylus">
 @require '../styles/wrapper.styl'
-
 .page-edit
   @extend $wrapper
   padding-top 1rem
   padding-bottom 1rem
   overflow auto
-
   .edit-link
     display inline-block
     a
@@ -122,7 +72,6 @@ export default {
     .time
       font-weight 400
       color #aaa
-
 @media (max-width: $MQMobile)
   .page-edit
     .edit-link
