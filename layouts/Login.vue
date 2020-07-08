038decae59d08dec1a51b61c6d8f7084e5a506df
Il Harper <afanyiyu@hotmail.com>
2020-07-08 11:06:22 +0800

    feat: Login Layout Update

--- layouts/Login.vue
+++ layouts/Login.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="theme-container" :class="pageClasses">
     <Navbar />
+    <div class="sb-container" @click="triggerOAuth">
+      <p class="sb-title">→ 使用 GitHub 登录</p>
+      <p class="sb-description">您的贡献将会被记录在 VBox 仓库内。</p>
+    </div>
+    <div class="sb-container" @click="triggerBot">
+      <p class="sb-title">→ 继续但不登录</p>
+      <p class="sb-description">您将会以匿名的方式继续。</p>
+    </div>
   </div>
 </template>
 
@@ -12,6 +20,30 @@ export default {
 
   components: {
     Navbar
+  },
+
+  methods: {
+    triggerOAuth() {},
+
+    triggerBot() {}
   }
 }
 </script>
+
+<style lang="stylus">
+.sb-container
+  margin 48px 0
+  padding 12px
+  border 1.5px solid $accentColor
+  border-radius 15px
+  transition box-shadow 0.2s ease-in-out
+  cursor pointer
+.sb-container:hover
+  box-shadow 0 0 24px 0 $accentColor
+.sb-title
+  margin 10px
+  font-size 24px
+  font-weight bold
+.sb-description
+  margin 8px 10px
+</style>
