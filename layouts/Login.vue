71498cb9dd80fe33efb5d22391c4877322bee6f4
Il Harper <afanyiyu@hotmail.com>
2020-07-08 11:59:49 +0800

    feat: Login Layout Update

--- layouts/Login.vue
+++ layouts/Login.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="theme-container" :class="pageClasses">
     <Navbar />
-    <div class="sb-container" @click="triggerOAuth">
-      <p class="sb-title">→ 使用 GitHub 登录</p>
-      <p class="sb-description">您的贡献将会被记录在 VBox 仓库内。</p>
-    </div>
-    <div class="sb-container" @click="triggerBot">
-      <p class="sb-title">→ 继续但不登录</p>
-      <p class="sb-description">您将会以匿名的方式继续。</p>
+    <div class="sb-container">
+      <div class="sb-button-container" @click="triggerOAuth">
+        <p class="sb-title">→ 使用 GitHub 登录</p>
+        <p class="sb-description">您的贡献将会被记录在 VBox 仓库内。</p>
+      </div>
+      <div class="sb-button-container" @click="triggerBot">
+        <p class="sb-title">→ 继续但不登录</p>
+        <p class="sb-description">您将会以匿名的方式继续。</p>
+      </div>
     </div>
   </div>
 </template>
@@ -32,13 +34,18 @@ export default {
 
 <style lang="stylus">
 .sb-container
-  margin 48px 0
+  margin 2rem
+  margin-top 3.6rem
+  padding 3rem
+.sb-button-container
+  margin 12px
+  margin-top 24px
   padding 12px
   border 1.5px solid $accentColor
   border-radius 15px
   transition box-shadow 0.2s ease-in-out
   cursor pointer
-.sb-container:hover
+.sb-button-container:hover
   box-shadow 0 0 24px 0 $accentColor
 .sb-title
   margin 10px
